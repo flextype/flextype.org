07c824b9d9ac63cef6ad820a3e89b324e1d35b08
Awilum <awilum@msn.com>
2018-03-18 23:41:47 +0300

    Themes Updates

--- site/themes/default/partials/head.php
+++ site/themes/default/partials/head.php
@@ -6,22 +6,23 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-	<?php $this->rawilum['events']->dispatch('theme_meta'); ?>
+	<?php Rawilum\Events::dispatch('onThemeMeta'); ?>
 
 	<link rel="shortcut icon" href="<?php echo Url::getBase(); ?>/favicon.ico">
 
-	<title><?php echo $this->rawilum['config']->get('site.title'); ?> | <?php echo $page['title']; ?></title>
+	<title><?php echo Rawilum\Config::get('site.title'); ?> | <?php echo $page['title']; ?></title>
 
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700' rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
 
     <!-- Bootstrap core CSS -->
-	<link href="<?php echo Url::getBase(); ?>/site/themes/<?php echo $this->rawilum['config']->get('site.theme'); ?>/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo Url::getBase(); ?>/site/themes/<?php echo Rawilum\Config::get('site.theme'); ?>/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-	<link href="<?php echo Url::getBase(); ?>/site/themes/<?php echo $this->rawilum['config']->get('site.theme'); ?>/assets/css/rawilum.css" rel="stylesheet">
-	<?php $this->rawilum['events']->dispatch('theme_header'); ?>
+	<link href="<?php echo Url::getBase(); ?>/site/themes/<?php echo Rawilum\Config::get('site.theme'); ?>/assets/css/rawilum.css" rel="stylesheet">
+	<?php Rawilum\Events::dispatch('onThemeHeader'); ?>
   </head>
   <body>
-  <?php $this->rawilum['themes']->getTemplate('partials/navigation'); ?>
+  <?php Rawilum\Templates::display('partials/navigation'); ?>
--- site/themes/default/partials/navigation.php
+++ site/themes/default/partials/navigation.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white">
+<nav class="navbar navbar-expand-lg navbar-light">
 <div class="container">
-  <a class="navbar-brand" href="<?php echo Url::getBase(); ?>"><img src="<?php echo Url::getBase(); ?>/site/themes/default/assets/img/logo.jpg" width=220 title="<?php echo $this->rawilum['config']->get('site.title'); ?>"></a>
+  <a class="navbar-brand" href="<?php echo Url::getBase(); ?>"><h1 class="h1">FLEXGINE</h1></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -13,7 +13,10 @@
         <a class="nav-link <?php if (Url::getUriSegment(0) == 'download') echo 'active'; ?>" href="<?php echo Url::getBase(); ?>/download">Download</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php if (Url::getUriSegment(0) == 'support') echo 'active'; ?>" href="<?php echo Url::getBase(); ?>/support">Support</a>
+        <a class="nav-link <?php if (Url::getUriSegment(0) == 'documentation') echo 'active'; ?>" href="<?php echo Url::getBase(); ?>/documentation">Documentation</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?php if (Url::getUriSegment(0) == 'support') echo 'active'; ?>" href="<?php echo Url::getBase(); ?>/support">Community</a>
       </li>
       <li class="nav-item">
         <a class="nav-link <?php if (Url::getUriSegment(0) == 'about') echo 'active'; ?>" href="<?php echo Url::getBase(); ?>/about">About</a>
