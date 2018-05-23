625ab0625950f0ce49f729c426280254292bb419
Awilum <awilum@msn.com>
2018-05-24 01:37:44 +0300

    fixing issues with comments

--- site/themes/default/views/partials/tail.php
+++ site/themes/default/views/partials/tail.php
@@ -2,7 +2,7 @@
     namespace Flextype;
     use Flextype\Component\{Event\Event, Http\Http, Registry\Registry};
 ?>
-<script id="dsq-count-scr" src="//flextype.org.disqus.com/count.js" async></script>
+<script id="dsq-count-scr" src="//flextype.disqus.com/count.js" async></script>
 <script src="<?php echo Http::getBaseUrl(); ?>/site/themes/<?php echo Registry::get('site.theme'); ?>/assets/js/jquery.min.js"></script>
 <script src="<?php echo Http::getBaseUrl(); ?>/site/themes/<?php echo Registry::get('site.theme'); ?>/assets/js/bootstrap.min.js"></script>
 <?php Event::dispatch('onThemeFooter'); ?>
