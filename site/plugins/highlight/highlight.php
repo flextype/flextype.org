8fefe1b32ae9d45ce5cbd62ceb49100191862bec
Awilum <awilum@msn.com>
2018-12-27 23:31:15 +0300

    Highlight plugins custom update

--- site/plugins/highlight/highlight.php
+++ site/plugins/highlight/highlight.php
@@ -14,23 +14,28 @@
 
 use Flextype\Component\Event\Event;
 use Flextype\Component\Registry\Registry;
+use Flextype\Component\Html\Html;
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 //
 // Add listner for onThemeHeader event
 //
 Event::addListener('onThemeHeader', function () {
-    echo('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/'.Registry::get('plugins.highlight.theme').'.min.css">');
+    echo('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/'.Registry::get('plugins.highlight.theme').'.min.css">');
 });
 
 //
 // Add listner for onThemeFooter event
 //
 Event::addListener('onThemeFooter', function () {
-    echo('<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    echo('<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/highlight.min.js"></script>
           <script>hljs.initHighlightingOnLoad();</script>');
 });
 
 Event::addListener('onShortcodesInitialized', function () {
+    Content::shortcode()->addHandler('code', function(ShortcodeInterface $s) {
+        return Html::toText($s->getContent());
+    });
     Content::shortcode()->addHandler('s_start', function() {
         return '[';
     });
