4a13a91b83f52132adb948baf1a875cbe8545e42
Awilum <awilum@msn.com>
2019-01-22 17:40:39 +0300

    Theme: about template updates

--- site/themes/flextype/views/templates/about.php
+++ site/themes/flextype/views/templates/about.php
@@ -1,6 +1,20 @@
 <?php namespace Flextype ?>
 <?php Themes::view('partials/head')->display() ?>
-<main role="main" class="container">
-    <?= $entry['content'] ?>
-</main>
+<div class="page-content container">
+    <h2 class="h2"><?= $entry['title'] ?></h2>
+    <div class="inner">
+        <nav>
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Flextype</a>
+            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Media</a>
+            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Sponsors</a>
+          </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade active show" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+              <?= $entry['content'] ?>
+          </div>
+        </div>
+    </div>
+</div>
 <?php Themes::view('partials/footer')->display() ?>
