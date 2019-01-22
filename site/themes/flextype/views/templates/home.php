f57f74a9e8fcae38614bbd0a68dc835fe3bda3bd
Awilum <awilum@msn.com>
2019-01-22 08:23:20 +0300

    Theme: home templates updates

--- site/themes/flextype/views/templates/home.php
+++ site/themes/flextype/views/templates/home.php
@@ -1,6 +1,4 @@
 <?php namespace Flextype ?>
 <?php Themes::view('partials/head')->display() ?>
-<main role="main" class="container">
-    <?= $entry['content'] ?>
-</main>
+<?= $entry['content'] ?>
 <?php Themes::view('partials/footer')->display() ?>
