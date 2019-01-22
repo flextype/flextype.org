d2f1a3a16a3ee6ce881288596d2bfff2964ddbab
Awilum <awilum@msn.com>
2019-01-22 20:25:38 +0300

    Theme: about template updates

--- site/themes/flextype/views/templates/about.php
+++ site/themes/flextype/views/templates/about.php
@@ -1,17 +1,20 @@
-<?php namespace Flextype ?>
+<?php
+namespace Flextype;
+use Flextype\Component\Http\Http;
+?>
 <?php Themes::view('partials/head')->display() ?>
 <div class="page-content container">
     <h2 class="h2"><?= $entry['title'] ?></h2>
     <div class="inner">
         <nav>
-          <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Flextype</a>
-            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Media</a>
-            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Sponsors</a>
-          </div>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+              <a class="nav-item nav-link <?php if ((\strpos(Http::getUriString(), 'media') === false) && (\strpos(Http::getUriString(), 'sponsors') === false)): ?>active<?php endif ?>" id="nav-home-tab" href="<?= Http::getBaseUrl() ?>/<?= Snippets::get('locale') ?>/about" role="tab" aria-controls="nav-about" aria-selected="true">About</a>
+              <a class="nav-item nav-link <?php if (\strpos(Http::getUriString(), 'media') !== false): ?>active<?php endif ?>" id="nav-profile-tab" href="<?= Http::getBaseUrl() ?>/<?= Snippets::get('locale') ?>/about/media" role="tab" aria-controls="nav-media" aria-selected="false">Media</a>
+              <a class="nav-item nav-link <?php if (\strpos(Http::getUriString(), 'sponsors') !== false): ?>active<?php endif ?>" id="nav-contact-tab" href="<?= Http::getBaseUrl() ?>/<?= Snippets::get('locale') ?>/about/sponsors" role="tab" aria-controls="nav-sponsors" aria-selected="false">Sponsors</a>
+            </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade active show" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+          <div class="tab-pane fade active show" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
               <?= $entry['content'] ?>
           </div>
         </div>
