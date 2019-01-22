2f32acd7d9228a5961d415795b74ed818682ee9a
Awilum <awilum@msn.com>
2019-01-22 16:28:09 +0300

    Theme: download templates updates

--- site/themes/flextype/views/templates/download.php
+++ site/themes/flextype/views/templates/download.php
@@ -3,9 +3,9 @@
 <div class="page-content container">
     <h2 class="h2">Download</h2>
     <div class="inner">
-        <?= $entry['content'] ?>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <center>
+            <?= $entry['content'] ?>
+        </center>
     </div>
 </div>
 <?php Themes::view('partials/footer')->display() ?>
