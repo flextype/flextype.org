75244d34c4d6af4ed45a21f5e90b607884d85197
Awilum <awilum@msn.com>
2019-01-26 20:53:49 +0300

    Themes: head - updates

--- site/themes/flextype/views/partials/head.php
+++ site/themes/flextype/views/partials/head.php
@@ -1,7 +1,7 @@
 <?php namespace Flextype ?>
 <?php use Flextype\Component\{Event\Event, Http\Http, Registry\Registry, Assets\Assets, Text\Text, Html\Html} ?>
 <!doctype html>
-<html lang="<?= Registry::get('settings.locale') ?>">
+<html lang="<?= locale() ?>">
   <head>
     <meta charset="<?= Text::lowercase(Registry::get('settings.charset')); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,6 +9,23 @@
     <meta name="keywords" content="<?= (isset($entry['keywords']) ? $entry['keywords'] : Registry::get('settings.keywords')) ?>">
     <meta name="robots" content="<?= (isset($entry['robots']) ? $entry['robots'] : Registry::get('settings.robots')) ?>">
     <meta name="generator" content="Powered by Flextype <?= Flextype::VERSION ?>" />
+
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@getflextype">
+    <meta name="twitter:creator" content="@getflextype">
+    <meta name="twitter:title" content="Content Management System &mdash; Flextype">
+    <meta name="twitter:description" content="Build fast, flexible, easier to manage websites with Flextype.">
+    <meta name="twitter:image" content="https://github.com/flextype/flextype/raw/dev/site/plugins/admin/preview.png">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="http://flextype.org">
+    <meta property="og:title" content="Content Management System &mdash; Flextype">
+    <meta property="og:description" content="Build fast, flexible, easier to manage websites with Flextype.">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://github.com/flextype/flextype/raw/dev/site/plugins/admin/preview.png">
+    <meta property="og:image:type" content="image/png">
 
 	<?php Event::dispatch('onThemeMeta') ?>
 
