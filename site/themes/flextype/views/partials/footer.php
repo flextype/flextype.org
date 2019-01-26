55714a4d22b9906cb0ca8983e79a8d7efe3a913a
Awilum <awilum@msn.com>
2019-01-27 00:02:26 +0300

    Themes: footer - updates

--- site/themes/flextype/views/partials/footer.php
+++ site/themes/flextype/views/partials/footer.php
@@ -41,6 +41,8 @@
                 <div class="col-md-12 footer-locales">
                     <p class="p">
                         <a href="<?= Http::getBaseUrl() ?>/en">English</a>
+                        <a href="<?= Http::getBaseUrl() ?>/de">Deutsch</a>
+                        <a href="<?= Http::getBaseUrl() ?>/fr">Français</a>
                         <a href="<?= Http::getBaseUrl() ?>/ru">Русский</a>
                     </p>
                 </div>
