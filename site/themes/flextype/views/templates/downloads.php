3d441cc122b170addd9f9da72fb1f0554eda7411
Awilum <awilum@msn.com>
2019-01-26 16:27:02 +0300

    Theme: downloads template updates

--- site/themes/flextype/views/templates/downloads.php
+++ site/themes/flextype/views/templates/downloads.php
@@ -9,16 +9,16 @@ use Flextype\Component\Http\Http;
         <nav>
           <div class="nav nav-tabs" id="nav-tab" role="tablist">
             <a class="nav-item nav-link <?php if ((\strpos(Http::getUriString(), 'plugins') === false) && (\strpos(Http::getUriString(), 'themes') === false)): ?>active<?php endif ?>" id="nav-home-tab" href="<?= Http::getBaseUrl() ?>/<?= Snippets::get('locale') ?>/downloads" role="tab" aria-controls="nav-home" aria-selected="true">Flextype</a>
-            <a class="nav-item nav-link <?php if (\strpos(Http::getUriString(), 'plugins') !== false): ?>active<?php endif ?>" id="nav-profile-tab" href="<?= Http::getBaseUrl() ?>/<?= Snippets::get('locale') ?>/downloads/plugins" role="tab" aria-controls="nav-profile" aria-selected="false"><?= $entry['text']['plugins'] ?></a>
-            <a class="nav-item nav-link <?php if (\strpos(Http::getUriString(), 'themes') !== false): ?>active<?php endif ?>" id="nav-contact-tab" href="<?= Http::getBaseUrl() ?>/<?= Snippets::get('locale') ?>/downloads/themes" role="tab" aria-controls="nav-contact" aria-selected="false"><?= $entry['text']['themes'] ?></a>
+            <a class="nav-item nav-link <?php if (\strpos(Http::getUriString(), 'plugins') !== false): ?>active<?php endif ?>" id="nav-profile-tab" href="<?= Http::getBaseUrl() ?>/<?= Snippets::get('locale') ?>/downloads/plugins" role="tab" aria-controls="nav-profile" aria-selected="false"><?= Entries::getEntry(locale())['text']['plugins'] ?></a>
+            <a class="nav-item nav-link <?php if (\strpos(Http::getUriString(), 'themes') !== false): ?>active<?php endif ?>" id="nav-contact-tab" href="<?= Http::getBaseUrl() ?>/<?= Snippets::get('locale') ?>/downloads/themes" role="tab" aria-controls="nav-contact" aria-selected="false"><?= Entries::getEntry(locale())['text']['themes'] ?></a>
           </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
           <div class="tab-pane text-center fade active show" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-              <a href="#" class="btn btn-download">Dowload Flextype</a>
+              <a href="https://github.com/flextype/flextype/releases/download/v<?= Flextype::VERSION ?>/flextype-<?= Flextype::VERSION ?>.zip" class="btn btn-download"><?= $entry['text']['download_flextype'] ?></a>
               <p>
-                  <?= $entry['text']['current_stable_version'] ?> 0.8.3<br>
-                  <a href="#"><?= $entry['text']['view_changelog'] ?></a>
+                  <?= $entry['text']['current_stable_version'] ?> <?= Flextype::VERSION ?><br>
+                  <a href="https://github.com/flextype/flextype/blob/master/CHANGELOG.md"><?= $entry['text']['view_changelog'] ?></a>
               </p>
           </div>
           <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
