34f01404b592f1ebfa431d4d0bf816a9563283d1
Awilum <awilum@msn.com>
2019-01-26 14:25:57 +0300

    Change simple to flextype

--- site/themes/flextype/gulpfile.js
+++ site/themes/flextype/gulpfile.js
@@ -11,24 +11,24 @@ var Promise = require("es6-promise").Promise,
     autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass');
 
-gulp.task('simple-css', function() {
-    return gulp.src('assets/scss/simple.scss')
+gulp.task('flextype-css', function() {
+    return gulp.src('assets/scss/flextype.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(csso())
-        .pipe(concat('default.min.css'))
+        .pipe(concat('flextype.min.css'))
         .pipe(gulp.dest('assets/dist/css/'));
 });
 
-gulp.task('js', function(){
+gulp.task('flextype-js', function(){
   return gulp.src(['node_modules/jquery/dist/jquery.slim.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
     .pipe(sourcemaps.init())
-    .pipe(concat('default.min.js'))
+    .pipe(concat('flextype.min.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('assets/dist/js/'));
 });
 
-gulp.task('default', ['simple-css', 'js']);
+gulp.task('default', ['flextype-css', 'flextype-js']);
