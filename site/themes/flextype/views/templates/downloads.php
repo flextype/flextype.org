2c586fb13c0a35bec5a4d5d0ec8850cfa3f857d2
Awilum <awilum@msn.com>
2019-06-18 21:02:54 +0300

    updates

--- site/themes/flextype/views/templates/downloads.php
+++ site/themes/flextype/views/templates/downloads.php
@@ -15,9 +15,9 @@ use Flextype\Component\Http\Http;
         </nav>
         <div class="tab-content" id="nav-tabContent">
           <div class="tab-pane text-center fade active show" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-              <a href="https://github.com/flextype/flextype/releases/download/v<?= Flextype::VERSION ?>/flextype-<?= Flextype::VERSION ?>.zip" class="btn btn-download"><?= $entry['text']['download_flextype'] ?></a>
+              <a href="https://github.com/flextype/flextype/releases/tag/v0.9.1" class="btn btn-download"><?= $entry['text']['download_flextype'] ?></a>
               <p>
-                  <?= $entry['text']['current_stable_version'] ?> 0.9.0<br>
+                  <?= $entry['text']['current_stable_version'] ?> 0.9.1<br>
                   <a href="https://github.com/flextype/flextype/blob/master/CHANGELOG.md"><?= $entry['text']['view_changelog'] ?></a>
               </p>
           </div>
