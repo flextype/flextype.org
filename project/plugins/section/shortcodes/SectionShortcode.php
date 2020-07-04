9a1b5cf43adffe06c6542fabe1da626c38db90e9
Awilum <awilum@msn.com>
2020-07-04 17:35:15 +0300

    update section plugin

--- project/plugins/section/shortcodes/SectionShortcode.php
+++ project/plugins/section/shortcodes/SectionShortcode.php
@@ -7,12 +7,10 @@ use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 // Shortcode: [section_create id=section-uid]Section content here[/section_create]
 $flextype['shortcodes']->addHandler('section_create', function (ShortcodeInterface $s) use ($flextype) {
-    $section_id = 'sections.' . $s->getParameter('id');
     $flextype->registry->set('sections.' . $s->getParameter('id'), $s->getContent());
 });
 
 // Shortcode: [section_fetch id=section-uid]
 $flextype['shortcodes']->addHandler('section_fetch', function (ShortcodeInterface $s) use ($flextype) {
-    $section_id = 'sections.' . $s->getParameter('id');
     return $flextype->registry->get('sections.' . $s->getParameter('id'));
 });
