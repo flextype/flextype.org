a29d29f885cefc461b836ecbe40ee41b6235bc48
Awilum <awilum@msn.com>
2020-12-24 17:34:28 +0300

    change locale detection method

--- project/themes/noir-flextype/bootstrap.php
+++ project/themes/noir-flextype/bootstrap.php
@@ -9,7 +9,6 @@ use Slim\Http\Uri;
 
 $scriptPath = PATH['project'] . '/themes/' . flextype('registry')->get('plugins.site.settings.theme') . '/scripts';
 
-include_once $scriptPath . '/locale.php';
 include_once $scriptPath . '/tokens.php';
 include_once $scriptPath . '/blog.php';
 include_once $scriptPath . '/dump.php';
