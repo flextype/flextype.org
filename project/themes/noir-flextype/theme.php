35689762640069fc98d2f4924a828361782ffcc5
Awilum <sergey.romanenko@flextype.org>
2021-03-13 14:04:12 +0300

    add seo redirects

--- project/themes/noir-flextype/theme.php
+++ project/themes/noir-flextype/theme.php
@@ -6,6 +6,8 @@ namespace Flextype\Themes\Noir;
 
 use Slim\Http\Environment;
 use Slim\Http\Uri;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 $scriptPath = PATH['project'] . '/themes/' . flextype('registry')->get('plugins.site.settings.theme') . '/scripts';
 
@@ -13,3 +15,18 @@ include_once $scriptPath . '/tokens.php';
 include_once $scriptPath . '/blog.php';
 include_once $scriptPath . '/dump.php';
 include_once $scriptPath . '/arrays.php';
+
+// Redirects for old urls with SEO 301
+flextype()->get('/en/{uri:.+}', function(Request $request, Response $response, $args) {
+    return $response->withRedirect('/' . $args['uri'], 301);
+});
+
+flextype()->get('/ru/{uri:.+}', function(Request $request, Response $response, $args) {
+    return $response->withRedirect('/' . $args['uri'], 301);
+});
+//---------
+
+// Redirect from home page to root
+flextype()->get('/home', function(Request $request, Response $response, $args) {
+    return $response->withRedirect('/' . $args['uri'], 301);
+});
